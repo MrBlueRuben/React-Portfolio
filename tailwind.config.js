9170aa581123fddc0ef72417b7e742fb6adfd532
ruben sanchez <sanchez.alvarez.ruben@gmail.com>
2023-04-08 18:44:22 +0200

    change montserrat font app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
 module.exports = {
   content: [
-
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-
+      fontFamily: {
+        mont: ['var(--font-mont)', ...fontFamily.sans],
       },
     },
   },
