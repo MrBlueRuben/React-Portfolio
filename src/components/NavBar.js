38f02535019fd5fd7467514170476b89a413f665
ruben sanchez <sanchez.alvarez.ruben@gmail.com>
2023-04-09 18:01:06 +0200

    navbar done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import React from 'react'
 import Logo from './Logo'
 import {useRouter} from "next/router";
+import { GithubIcon, LinkedInIcon } from './icons';
+import { motion } from "framer-motion";
 
 const CustomLink = ({href, title, className=""}) => {
   const router = useRouter();
@@ -26,10 +28,13 @@ const NavBar = () => {
         <CustomLink href="/articles" title="Articles" className='ml-4'/>
       </nav>
 
-      <nav>
-        <Link href="/" target={'_blank'}>G</Link>
-        <Link href="/" target={'_blank'}>G</Link>
-        <Link href="/" target={'_blank'}>G</Link>
+      <nav className='flex items-center justify-center flex-wrap'>
+        <motion.a href="https://github.com/MrBlueRuben" target={'_blank'}
+        whileHover={{y:-2, scale: 1.2,}} whileTap={{scale: 0.9}}
+        className='w-6 mr-3'><GithubIcon /></motion.a>
+        <motion.a href="https://www.linkedin.com/in/rubensanchezalvarez/" target={'_blank'}
+        whileHover={{y:-2, scale: 1.2,}} whileTap={{scale: 0.9}}
+        className='w-6 mr-3'><LinkedInIcon /></motion.a>
       </nav>
       <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
         <Logo />
