c0f22506a711b000fd5fddd373a24da24249e8f8
ruben sanchez <sanchez.alvarez.ruben@gmail.com>
2023-04-19 09:49:54 +0200

    hire me button working

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { CircularText } from './Icons'
+import Link from 'next/link'
 
 const HireMe = () => {
   return (
@@ -8,6 +9,7 @@ const HireMe = () => {
     '>
   <div className='w-48 h-auto flex items-center justify-center relative'>
   <CircularText className={"fill-dark animate-spin-slow"}/>
+  <Link href="mailto:sanchez.alvarez.ruben@gmail.com" className='flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border-solid border-dark h-20 w-20 rounded-full font-semibold hover:bg-light hover:text-dark'>Hire Me</Link>
   </div></div>
   )
 }
